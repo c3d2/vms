023d07321634104a8c62b984d10a07867a0632b8
Gilbert Röhrbein <payload@lavabit.com>
2014-01-18 23:24:20 +0100

    changed: use purecss.io instead of bootstrap and normalize

--- Foundation.hs
+++ Foundation.hs
@@ -71,7 +71,7 @@ instance Yesod App where
 
         pc <- widgetToPageContent $ do
             $(combineStylesheets 'StaticR
-                [ css_normalize_css
+                [ css_pure_css
                 ])
             $(widgetFile "default-layout")
         giveUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
