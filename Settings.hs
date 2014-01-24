f007d855d6580dc80844ca3789875e3a67546396
Astro <astro@spaceboyz.net>
2014-01-25 00:27:35 +0100

    Revert "changed: use local sqlite db, instead of remote postgresql"
    This reverts commit 6f2b266469a8db4dc8e901f1e4e22cfbaa66f3a6.

--- Settings.hs
+++ Settings.hs
@@ -9,7 +9,6 @@ import Prelude
 import Text.Shakespeare.Text (st)
 import Language.Haskell.TH.Syntax
 import Database.Persist.Postgresql (PostgresConf)
-import Database.Persist.Sqlite (SqliteConf)
 import Yesod.Default.Config
 import Yesod.Default.Util
 import Data.Text (Text)
@@ -20,7 +19,7 @@ import Data.Default (def)
 import Text.Hamlet
 
 -- | Which Persistent backend this site is using.
-type PersistConf = SqliteConf --PostgresConf
+type PersistConf = PostgresConf
 
 -- Static setting below. Changing these requires a recompile
 
